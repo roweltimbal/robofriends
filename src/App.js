9dc3ec5014c137ffbd0da123b432359685af05ea
PracticeBetter <practice_practice25@yahoo.com>
2024-03-30 19:47:23 -0400

    updated code for robofriends 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+
+  fetchData = async function () {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users#"
+      );
+      if (!response.ok) {
+        throw new Error("Could not fetch data");
+      }
+      const data = response.json();
+      return data;
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  async componentDidMount() {
+    const users = await this.fetchData();
+    this.setState({ robots: users });
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+
+    if (this.state.robots.length === 0) {
+      return <h1>Loading...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="teko-title f1">robofriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />;
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Eugene YuYu",
+    username: "Bret",
+    email: "Sincere@april.biz",
+  },
+  {
+    id: 2,
+    name: "Dennis Hakusho",
+    username: "Antonette",
+    email: "Shann@melissa.tv",
+  },
+  {
+    id: 3,
+    name: "Baki Hanma",
+    username: "Samantha",
+    email: "Natha@yesenia.net",
+  },
+  {
+    id: 4,
+    name: "Ippo Makunuchi",
+    username: "Karianne",
+    email: "Julianna.OConner@kory.biz",
+  },
+  {
+    id: 5,
+    name: "Capeta Racer",
+    username: "Kamren",
+    email: "Jester@april.biz",
+  },
+  {
+    id: 6,
+    name: "Luffy Onepiece",
+    username: "Luffy",
+    email: "Jester@april.biz",
+  },
+  {
+    id: 7,
+    name: "Shigeno Goro",
+    username: "Goro",
+    email: "Jester@april.biz",
+  },
+  {
+    id: 8,
+    name: "Hanamichi Sakuragi",
+    username: "Sakuragi",
+    email: "Jester@april.biz",
+  },
+  {
+    id: 9,
+    name: "Kaede Rukawa",
+    username: "Super Rookie",
+    email: "Jester@april.biz",
+  },
+  {
+    id: 10,
+    name: "Uchiha Itachi",
+    username: "Ninja",
+    email: "Jester@april.biz",
+  },
+];
